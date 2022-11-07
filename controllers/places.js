28c7247bf023038e8c583abd3dc7f725628e6f5a
Klyrasel <KaiLyrasel@outlook.com>
2022-11-06 19:35:09 -0500

    commit routes connecting

--- controllers/places.js
+++ controllers/places.js
@@ -5,8 +5,21 @@ router.get('/new', (req, res) => {
   res.render('places/new')
 })
 
+router.get('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    res.render('places/show', { place: places[id] })
+  }
+})
+
+
 router.post('/', (req, res) => {
-  console.log(req.body)
   if (!req.body.pic) {
     // Default image if one is not provided
     req.body.pic = 'http://placekitten.com/400/400'
